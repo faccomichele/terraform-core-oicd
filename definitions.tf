0d8b54fe670b0521e27e6f240a63a3240f83d892
Michele Facco <faccomichele@gmail.com>
2025-12-24 14:28:53 +0100

    test 003b

--- definitions.tf
+++ definitions.tf
@@ -1,5 +1,5 @@
 locals {
   environment   = split(terraform.workspace, "_")[0]
   aws_region    = split(terraform.workspace, "_")[1]
-  project_name  = "oidc-provider"
+  project_name  = "oidc-provider-${local.environment}-${local.aws_region}"
 }
