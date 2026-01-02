db7d7287ec8b7251c82c7a1e34b742820024c96a
Michele Facco <faccomichele@gmail.com>
2026-01-02 13:55:42 +0700

    removing s3 file as empty

--- s3.tf
+++
@@ -1,4 +0,0 @@
-/* S3 resources removed.
-   This file kept as placeholder to avoid accidental re-adding S3 resources.
-   To fully remove state entries, run `terraform state rm` for any existing S3 resources.
-*/
